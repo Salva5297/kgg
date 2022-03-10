e577461e9a5dca60544330971a1421fb4c7e2212
Salva5297 <salgonger1997@gmail.com>
2022-03-10 15:02:22 +0100

    Errors importing classes paths

--- Services/mappings_generator.py
+++ Services/mappings_generator.py
@@ -12,8 +12,7 @@ class Mappings_generator:
         """
         # Generate the mappings file
         with open('Repositories/mappings' + self.mappings_path + '/map.rml.ttl', 'w') as map_file:
-            map_file.write("""
-@prefix rr: <http://www.w3.org/ns/r2rml#> .
+            map_file.write("""@prefix rr: <http://www.w3.org/ns/r2rml#> .
 @prefix rml: <http://semweb.mmlab.be/ns/rml#> .
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
